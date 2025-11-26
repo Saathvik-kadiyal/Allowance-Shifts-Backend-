9b7f437f3aa74f5e42f06e6e45ac49751e63caf3
jathin-vignesh <170806523+jathin-vignesh@users.noreply.github.com>
2025-11-26 17:35:44 +0530

    changes in summary_route the latest possible date in db will come if null was given as params

--- app/routers/summary_routes.py
+++ app/routers/summary_routes.py
@@ -4,22 +4,24 @@ from db import get_db
 from utils.dependencies import get_current_user
 from services.summary_service import get_client_shift_summary
 from schemas.displayschema import ClientSummary
+from models.models import ShiftAllowances
 
 router = APIRouter(prefix="/summary", tags=["Summary"])
 
 
 @router.get("/client-shift-summary", response_model=list[ClientSummary])
 def client_shift_summary(
-    payroll_month: str,
+    payroll_month: str | None = None,
     db: Session = Depends(get_db),
     current_user=Depends(get_current_user),
 ):
-
     if not payroll_month:
-        raise HTTPException(
-            status_code=400,
-            detail="payroll_month is required. Example: 2025-01"
-        )
+        latest_date = db.query(ShiftAllowances.payroll_month).order_by(ShiftAllowances.payroll_month.desc()).first()
+        
+        if not latest_date or not latest_date[0]:
+            raise HTTPException(status_code=404, detail="No payroll data available in the system")
+
+        payroll_month = latest_date[0].strftime("%Y-%m")
 
     summary = get_client_shift_summary(db, payroll_month)
 
